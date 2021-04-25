2a34220a41227c362fce895c7502e45e7f9c3c00
Sungjoon Park <sungjoon512@gmail.com>
2021-04-25 23:36:12 +0900

    basic stuff

--- src/app.tsx
+++ src/app.tsx
@@ -1,7 +1,11 @@
 import React from 'react'
 
-const App: React.FC = () => {
-  return <div className='App'>app</div>
+export const App: React.FC = () => {
+  return (
+    <div className='App'>
+      <h1>Playlist Sharer</h1>
+      <p>Playlist Sharer is a social app that enables you to share your shopping lists with friends in realtime.</p>
+      <button type='button'>Sign In With Google</button>
+    </div>
+  )
 }
-
-export default App
