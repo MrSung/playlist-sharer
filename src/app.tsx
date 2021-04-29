e87c1f21ca3eef19ac192a315fb7db413062ef37
Sungjoon Park <sungjoon512@gmail.com>
2021-04-29 19:16:32 +0900

    add form to post

--- src/app.tsx
+++ src/app.tsx
@@ -1,10 +1,74 @@
-import React from 'react'
+import React, { useReducer, useState } from 'react'
 
 import * as db from './db'
 import * as hooks from './hooks'
 
+interface IInitialFormState {
+  title: string
+  artist: string
+  album: string
+}
+
+const initialFormState: IInitialFormState = {
+  title: '',
+  artist: '',
+  album: '',
+}
+
+enum ActionType {
+  SET_TITLE = 'SET_TITLE',
+  SET_ARTIST = 'SET_ARTIST',
+  SET_ALBUM = 'SET_ALBUM',
+}
+
+interface ISetTitle {
+  type: ActionType.SET_TITLE
+  payload: {
+    title: IInitialFormState['title']
+  }
+}
+
+interface ISetArtist {
+  type: ActionType.SET_ARTIST
+  payload: {
+    artist: IInitialFormState['artist']
+  }
+}
+
+interface ISetAlbum {
+  type: ActionType.SET_ALBUM
+  payload: {
+    album: IInitialFormState['album']
+  }
+}
+
+type Action = ISetTitle | ISetArtist | ISetAlbum
+
+const reducer: React.Reducer<IInitialFormState, Action> = (state, action) => {
+  switch (action.type) {
+    case ActionType.SET_TITLE:
+      return {
+        ...state,
+        title: action.payload.title,
+      }
+    case ActionType.SET_ARTIST:
+      return {
+        ...state,
+        artist: action.payload.artist,
+      }
+    case ActionType.SET_ALBUM:
+      return {
+        ...state,
+        album: action.payload.album,
+      }
+    default:
+      throw new Error()
+  }
+}
+
 export const AuthApp: React.FC = () => {
-  const [collection, setCollection] = React.useState<db.Collection | null>(null)
+  const [collection, setCollection] = useState<db.Collection | null>(null)
+  const [formState, dispatch] = useReducer(reducer, initialFormState)
 
   const onClickSignOut = () => {
     db.signOut()
@@ -25,9 +89,55 @@ export const AuthApp: React.FC = () => {
         Playlist Sharer is a social app that enables you to share your playlist
         with friends in realtime.
       </p>
-      <button type="button" onClick={onClickSignOut}>
+      <button type='button' onClick={onClickSignOut}>
         Sign Out
       </button>
+      <hr />
+      <form autoComplete='off' style={{ display: 'flex', alignItems: 'flex-end' }}>
+        <label>
+          <span>Title: </span>
+          <input
+            type='text'
+            name='title'
+            value={formState.title}
+            onChange={(ev) => {
+              dispatch({
+                type: ActionType.SET_TITLE,
+                payload: { title: ev.target.value },
+              })
+            }}
+          />
+        </label>
+        <label>
+          <span>Artist: </span>
+          <input
+            type='text'
+            name='artist'
+            value={formState.artist}
+            onChange={(ev) => {
+              dispatch({
+                type: ActionType.SET_ARTIST,
+                payload: { artist: ev.target.value },
+              })
+            }}
+          />
+        </label>
+        <label>
+          <span>Album: </span>
+          <input
+            type='text'
+            name='album'
+            value={formState.album}
+            onChange={(ev) => {
+              dispatch({
+                type: ActionType.SET_ALBUM,
+                payload: { album: ev.target.value },
+              })
+            }}
+          />
+        </label>
+        <button type='submit' style={{ marginBottom: '10px' }}>Add song</button>
+      </form>
       <hr />
       <table>
         <thead>
@@ -61,7 +171,7 @@ export const UnAuthApp: React.FC = () => {
   return (
     <div>
       <p>You are signed out.</p>
-      <button type="button" onClick={onClickSignInWithGoogle}>
+      <button type='button' onClick={onClickSignInWithGoogle}>
         Sign In With Google
       </button>
     </div>
