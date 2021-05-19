8f5d96a3db5f34dab1befd7a69eaf6021c19d919
Sungjoon Park <sungjoon512@gmail.com>
2021-05-19 23:49:28 +0900

    add hooks and markup for songs to show

--- src/components/pages/list-item.tsx
+++ src/components/pages/list-item.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import { useParams } from 'react-router-dom'
 
+import { useLoading, useGetPlaylistSongs } from 'src/hooks'
 import { Header, Form } from 'src/components/parts'
 
 export const ListItem: React.FC = () => {
   const { id } = useParams<{ id: string }>()
+
+  const { songs, error } = useGetPlaylistSongs(id)
+  const loadingString = useLoading()
 
   return (
     <div>
@@ -21,6 +25,53 @@ export const ListItem: React.FC = () => {
             <th />
           </tr>
         </thead>
+        <tbody>
+          {(() => {
+            if (error) {
+              return (
+                <tr>
+                  <td colSpan={5}>{error.message}</td>
+                </tr>
+              )
+            }
+
+            if (typeof songs === 'undefined') {
+              return (
+                <tr>
+                  <td colSpan={5}>{loadingString}</td>
+                </tr>
+              )
+            }
+
+            if (songs.length === 0) {
+              return (
+                <tr>
+                  <td colSpan={5}>There are no songs.</td>
+                </tr>
+              )
+            }
+
+            return songs
+              .sort((a, b) => a.index - b.index)
+              .map((o) => (
+                <tr key={o.id}>
+                  <td>{o.index}</td>
+                  <td>{o.title}</td>
+                  <td>{o.artist}</td>
+                  <td>{o.album}</td>
+                  <td>
+                    <button
+                      type='button'
+                      onClick={async () => {
+                        // TODO: delete song
+                      }}>
+                      &times;
+                    </button>
+                  </td>
+                </tr>
+              ))
+          })()}
+        </tbody>
       </table>
     </div>
   )
