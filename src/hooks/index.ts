d4ee4bbea894d93145dfcb3a68e30dd76511fb71
Sungjoon Park <sungjoon512@gmail.com>
2021-05-24 22:42:45 +0900

    categorize hooks

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,4 +1,5 @@
-export { useUser } from './use-user'
-export { useLoading } from './use-loading'
-export { useGetPlaylists } from './use-get-playlists'
-export { useGetPlaylistSongs } from './use-get-playlist-songs'
+export { useUser } from './db/use-user'
+export { useGetPlaylists } from './db/use-get-playlists'
+export { useGetPlaylistSongs } from './db/use-get-playlist-songs'
+
+export { useLoading } from './utils/use-loading'
