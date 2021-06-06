3f85845b27b5b44f08da9c79abae5daedab48e98
Sungjoon Park <sungjoon512@gmail.com>
2021-06-06 22:17:33 +0900

    place photo url

--- src/components/parts/header.tsx
+++ src/components/parts/header.tsx
@@ -1,29 +1,69 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { Link, useRouteMatch } from 'react-router-dom'
 
+import { UserContext } from 'src/app'
 import * as db from 'src/db'
 
 export const Header = () => {
   const isPlaylistItemRoute = useRouteMatch('/playlist/:id')
+  const loggedInUser = useContext(UserContext)
 
   return (
     <>
-      <header>
-        <h1>Playlist Sharer</h1>
-        <p>
+      <header
+        style={{
+          display: 'grid',
+          gridTemplateColumns: '1fr 100px',
+          columnGap: '10px',
+        }}>
+        <h1 style={{ gridRow: '1 / 2', gridColumn: '1 / 2' }}>
+          Playlist Sharer
+        </h1>
+        {loggedInUser !== null && (
+          <span
+            style={{
+              gridRow: '1 / 2',
+              gridColumn: '2 / 3',
+              width: '96px',
+              height: '96px',
+              borderRadius: '50%',
+              overflow: 'hidden',
+            }}>
+            <img
+              src={loggedInUser.photoURL ?? 'https://via.placeholder.com/96'}
+              alt=''
+              width='96'
+              height='96'
+            />
+          </span>
+        )}
+        <p style={{ gridRow: '2 / 3', gridColumn: '1 / 2' }}>
           Playlist Sharer is a social app that enables you to share playlists
           according to your favorite song with friends in realtime.
         </p>
-        <div style={{ display: 'flex', justifyContent: 'space-between' }}>
-          {isPlaylistItemRoute && <Link to='/'>&larr; Go back</Link>}
+        {isPlaylistItemRoute && (
+          <span
+            style={{
+              gridRow: '3 / 4',
+              gridColumn: '1 / 2',
+            }}>
+            <Link to='/'>&larr; Go back</Link>
+          </span>
+        )}
+        <div
+          style={{
+            gridRow: '3 / 4',
+            gridColumn: '2 / 3',
+            display: 'flex',
+            justifyContent: 'center',
+          }}>
           <a
             href=''
             onClick={(ev: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
               ev.preventDefault()
 
               db.signOut()
-            }}
-            style={{ marginLeft: 'auto' }}>
+            }}>
             Sign Out
           </a>
         </div>
